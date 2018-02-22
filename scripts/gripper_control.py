997df03d0404c78b509fa25467b2f65c0d57a169
Louise Gertz <louise.emy.gertz@gmail.com>
2018-02-22 05:47:06 -0600

    Added a publisher to the CModelOutput topic

--- scripts/gripper_control.py
+++ scripts/gripper_control.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python
-#import roslib; roslib.load_manifest('robotiq_c_model_control')
-#from robotiq_c_model_control.msg import _CModel_robot_output  as outputMsg
+import roslib; roslib.load_manifest('robotiq_c_model_control')
+from robotiq_c_model_control.msg import _CModel_robot_output  as outputMsg
 import rospy
 from std_msgs.msg import UInt8 
 
+
+def genCommand(data, command):  
+    #command = outputMsg.CModel_robot_output();
+    if data.data==4:    #activate
+        command.rACT = 1
+        command.rGTO = 1
+        command.rSP  = 105
+        command.rFR  = 25
+
+    if data.data==1:   #oppnar
+        command.rPR = 0
+
+  
     
 def gripper_control_sub():
-
     rospy.init_node('gripper_control', anonymous=True)
     rospy.Subscriber("myo_gest", UInt8, callback)
     rospy.spin()
 
-pub = rospy.Publisher('helloworld', UInt8)
-#pub = rospy.Publisher('CModelRobotOutput', outputMsg.CModel_robot_output)
+pub = rospy.Publisher('CModelRobotOutput', outputMsg.CModel_robot_output, queue_size=10)
+
+command = outputMsg.CModel_robot_output()
+gest = 0
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    pub.publish(7)
-
+    gest = data.data
+    genCommand(data, command)
+    pub.publish(command)
 
 if __name__ == '__main__':
     gripper_control_sub()
